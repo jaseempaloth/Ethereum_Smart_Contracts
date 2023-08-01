0a94b73b4788ad87cdb2e4408b40842de528b1e9
Jaseem Paloth <jaseempalothjp@email.com>
2023-08-01 22:17:57 +0530

    Employee Onboarding and Project Allocation

---
+++ Employee Onboarding and Project Allocation/Onboarding.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+abstract contract Onboarding {
+
+    struct Employee {
+        uint256 id;
+        string name;
+        string email;
+        uint256 salary;
+    }
+
+    Employee public employee;
+
+    function join(uint256 _id, string memory _name, string memory _email, uint256 _salary) public virtual {
+        employee = Employee(_id,_name, _email, _salary);
+    }
+
+    function nextSteps() public view virtual returns (string memory);
+}
+
+contract Allocation is Onboarding {
+
+    function allocate(string memory _employeeId, string memory _projectId) public view returns (string memory) {
+        string memory text = string.concat("The Employee with id: ", _employeeId, ", is added to project with id: ", _projectId );
+        nextSteps();
+        return text;
+    }
+
+    function nextSteps() public view override returns (string memory) {
+        return sendWelcomeEmail();
+    }
+
+    function sendWelcomeEmail() internal view returns (string memory)  {
+        return string.concat("Send welcome email to employee with email:", employee.email);
+    }
+}
